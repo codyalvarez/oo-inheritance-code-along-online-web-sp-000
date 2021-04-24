e3cbb4993fde265d7c4ad81e4198526ebf4b7698
Cody Alvarez <cody_alvarez19@outlook.com>
2021-04-24 23:14:01 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -3,14 +3,14 @@ class Vehicle
 
   def initialize(wheel_size, wheel_number)
     @wheel_size = wheel_size
-    @wheel_number = wheel_number 
+    @wheel_number = wheel_number
   end
 
-  def go 
+  def go
     "vrrrrrrrooom!"
   end
 
-  def fill_up_tank 
+  def fill_up_tank
     "filling up!"
   end
 end
